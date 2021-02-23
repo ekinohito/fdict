bb183322843b9194ea33f59923ac3ee605d983bf
ekinohito <53277590+ekinohito@users.noreply.github.com>
2021-02-23 15:54:56 +0300

    added context management

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-class FDict(dict):
-    def __init__(self, f, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class FDict:
+    def __init__(self, f):
+        self.contexts = [{}]
         self.f = f
 
+    def __setitem__(self, key, value):
+        self.contexts[-1][key] = value
+
     def __getitem__(self, item):
-        if item in self:
-            return super().__getitem__(item)
-        else:
-            return self.f(item)
+        for context in reversed(self.contexts):
+            if item in context:
+                return context[item]
+        return self.f(item)
+
+    def __enter__(self):
+        self.contexts.append({})
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.contexts.pop()
 
 
-def fdict(f):
-    return FDict(f)
-
-
-@fdict
+@FDict
 def test(x):
     return x % 2 == 0
 
 
 if __name__ == '__main__':
     test[0] = False
-    for i in range(10):
+    for i in range(5):
+        print(i, ': ', test[i])
+
+    with test:
+        test[3] = True
+        for i in range(5):
+            print(i, ': ', test[i])
+
+    for i in range(5):
         print(i, ': ', test[i])
